7fe0949bce00e2b07440b7fcebb13832c729910d
Bruno Viana <bbviana@touchhealth.com.br>
2019-09-08 12:11:39 -0300

    genesis

---
+++ src/main/java/br/com/stilingue/lacio/LacioApplication.java
@@ -0,0 +1,13 @@
+package br.com.stilingue.lacio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LacioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/stilingue/lacio/LacioApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.stilingue.lacio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LacioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
